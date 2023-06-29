74b039aeaba440a2a33ba356996d2dc1e5048c2d
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-06-29 12:18:52 +0400

    /

---
+++ StudentFormDesktop/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using StudentFormDesktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentFormDesktop.Data
+{
+    public static class ApplicationDbContext
+    {
+        public static List<Student> Students = new List<Student>();
+
+        static ApplicationDbContext()
+        {
+            Students = new List<Student>();
+            Student student = new Student();
+            
+        }
+    }
+}
---
+++ StudentFormDesktop/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentFormDesktop.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Faculty { get; set; }
+        public string Profession { get; set; }
+
+    }
+}
