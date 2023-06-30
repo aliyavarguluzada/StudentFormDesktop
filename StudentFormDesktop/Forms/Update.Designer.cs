265c8967659e5bd1c6fe957b7cc9c5022655e740
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-06-30 13:17:20 +0400

    Update

---
+++ StudentFormDesktop/Forms/Update.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace StudentFormDesktop.Forms
+{
+    partial class Update
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Update_btn = new Button();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            txbx_Update_Profession = new TextBox();
+            txbx_Update_Faculty = new TextBox();
+            txbx_Update_Address = new TextBox();
+            txbx_Update_Email = new TextBox();
+            txbx_Update_Surname = new TextBox();
+            txbx_Update_Name = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // Update_btn
+            // 
+            Update_btn.Location = new Point(387, 318);
+            Update_btn.Name = "Update_btn";
+            Update_btn.Size = new Size(94, 29);
+            Update_btn.TabIndex = 24;
+            Update_btn.Text = "Update";
+            Update_btn.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(64, 358);
+            label6.Name = "label6";
+            label6.Size = new Size(77, 20);
+            label6.TabIndex = 23;
+            label6.Text = "Profession";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(64, 295);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 20);
+            label5.TabIndex = 22;
+            label5.Text = "Faculty";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(64, 231);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 20);
+            label4.TabIndex = 21;
+            label4.Text = "Address";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(64, 160);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 20;
+            label3.Text = "Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 19;
+            label2.Text = "Surname";
+            // 
+            // txbx_Update_Profession
+            // 
+            txbx_Update_Profession.Location = new Point(64, 381);
+            txbx_Update_Profession.Name = "txbx_Update_Profession";
+            txbx_Update_Profession.Size = new Size(132, 27);
+            txbx_Update_Profession.TabIndex = 18;
+            // 
+            // txbx_Update_Faculty
+            // 
+            txbx_Update_Faculty.Location = new Point(64, 318);
+            txbx_Update_Faculty.Name = "txbx_Update_Faculty";
+            txbx_Update_Faculty.Size = new Size(132, 27);
+            txbx_Update_Faculty.TabIndex = 17;
+            // 
+            // txbx_Update_Address
+            // 
+            txbx_Update_Address.Location = new Point(64, 254);
+            txbx_Update_Address.Name = "txbx_Update_Address";
+            txbx_Update_Address.Size = new Size(132, 27);
+            txbx_Update_Address.TabIndex = 16;
+            // 
+            // txbx_Update_Email
+            // 
+            txbx_Update_Email.Location = new Point(64, 183);
+            txbx_Update_Email.Name = "txbx_Update_Email";
+            txbx_Update_Email.Size = new Size(132, 27);
+            txbx_Update_Email.TabIndex = 15;
+            // 
+            // txbx_Update_Surname
+            // 
+            txbx_Update_Surname.Location = new Point(64, 116);
+            txbx_Update_Surname.Name = "txbx_Update_Surname";
+            txbx_Update_Surname.Size = new Size(132, 27);
+            txbx_Update_Surname.TabIndex = 14;
+            // 
+            // txbx_Update_Name
+            // 
+            txbx_Update_Name.Location = new Point(64, 51);
+            txbx_Update_Name.Name = "txbx_Update_Name";
+            txbx_Update_Name.Size = new Size(132, 27);
+            txbx_Update_Name.TabIndex = 13;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(69, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 25;
+            label1.Text = "Name";
+            // 
+            // Update
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(Update_btn);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txbx_Update_Profession);
+            Controls.Add(txbx_Update_Faculty);
+            Controls.Add(txbx_Update_Address);
+            Controls.Add(txbx_Update_Email);
+            Controls.Add(txbx_Update_Surname);
+            Controls.Add(txbx_Update_Name);
+            Name = "Update";
+            Text = "Update";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Update_btn;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox txbx_Update_Profession;
+        private TextBox txbx_Update_Faculty;
+        private TextBox txbx_Update_Address;
+        private TextBox txbx_Update_Email;
+        private TextBox txbx_Update_Surname;
+        private TextBox txbx_Update_Name;
+        private Label label1;
+    }
+}
